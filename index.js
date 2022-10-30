af15bf8a078c2824dd1c35bf9edd7266508eb783
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-10-30 12:53:45 -0700

    new commit

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.listen(3000)
-
-// commit demo //
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
@@ -22,3 +13,4 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
+
