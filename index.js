3d759e2002d0881c7e645da8aa417b12c5ea64bc
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-10-30 12:42:15 -0700

    commit again

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000)
+
+// commit demo //
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(process.env.PORT)
