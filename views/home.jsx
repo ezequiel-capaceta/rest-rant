e3589193ccdc8ccd4640a36abe1a6bc7b42b62b8
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-11-06 01:27:02 -0700

    Adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -1,20 +1,23 @@
 const React = require('react')
 const Def = require('./default')
 
-
 function home () {
-  return (
-    <Def>
-        <main>
-            <h1>HOME</h1>
-        </main>
-        <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
-    </Def>
-  )
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/food-oh-yes.jpg" alt="food" />
+                  <div>
+                    Photo by <a href="https://unsplash.com/@goodeats_yqr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">GoodEats YQR</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
 }
 
-  
 module.exports = home
