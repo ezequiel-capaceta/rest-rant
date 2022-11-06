704bfff045312dfea8422d58cfcfc74526116530
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-11-06 01:47:23 -0700

    Editing on CSS

--- views/home.jsx
+++ views/home.jsx
@@ -5,8 +5,8 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
-                <div>
+                <h1>REST-Rant</h1>
+                <div class='food_img'>
                   <img src="/images/food-oh-yes.jpg" alt="food" />
                   <div>
                     Photo by <a href="https://unsplash.com/@goodeats_yqr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">GoodEats YQR</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
