6471d630a1a88cdf1ad6666558d6f804176fb644
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-11-06 01:27:50 -0800

    another commit

--- controllers/places.js
+++ controllers/places.js
@@ -25,23 +25,7 @@ router.post('/', (req, res) => {
 router.get('/new', (req, res) => {
   res.render('places/new')
   })
-  
 
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
-  }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: '/images/coffee-cat.jpg'
-  }]
-  
-  
   res.render('places/index', { places })
 })
 
