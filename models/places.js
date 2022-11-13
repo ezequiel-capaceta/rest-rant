374c1a0d586eea9fc192d4f152de28ef142c1de6
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-11-13 13:46:58 -0800

    Updating Index and Show

--- models/places.js
+++ models/places.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+module.exports.Place = require('./places')
+
 module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
