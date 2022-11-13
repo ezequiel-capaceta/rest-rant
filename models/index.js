8f8db89690ef13fe9a7b85b0637d93a4f65f28ec
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-11-13 14:00:02 -0800

    Fixing show page and links

--- models/index.js
+++ models/index.js
@@ -5,3 +5,4 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 
+module.exports.Place = require('./places')
--- models/places.js
+++ models/places.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
 })
 
-module.exports.Place = require('./places')
-
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
-}]
+module.exports = mongoose.model('Place', placeSchema)
